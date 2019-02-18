702738190c1e1e53daa45c728324398c375c9fe6
Andrey Popov <andpop@mail.ru>
2019-02-19 00:05:43 +0300

    Work1/task1. Copy files without erasing.

--- work1/task1.js
+++ work1/task1.js
@@ -5,22 +5,50 @@ const folders = {
   output: ''
 };
 
+const NO_INPUT_FOLDER_ARGUMENT = 1;
+const NO_OUTPUT_FOLDER_ARGUMENT = 2;
+const NO_INPUT_FOLDER = 3;
+const OUTPUT_FOLDER_CREATE_ERROR = 4;
+
+function getOutFolderForFile (filename) {
+  let folderName = filename.charAt(0).toUpperCase();
+  return path.join(folders.output, folderName);
+}
+
+function copyFile (fullFilename) {
+  let outFolder, outputFileName;
+  let file = path.basename(fullFilename);
+  outFolder = getOutFolderForFile(file);
+  outputFileName = path.join(outFolder, file);
+  if (!fs.existsSync(outFolder)) {
+    try {
+      fs.mkdirSync(outFolder);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+  fs.copyFileSync(fullFilename, outputFileName);
+
+  console.log(outputFileName);
+}
+
 function getAllFiles (folderName) {
   fs.readdir(folderName, (err, files) => {
     if (err) {
-      console.error('Ошибка чтения каталога');
+      console.error(`Input folder (${folders.input}) read error.`);
       throw err;
     }
 
     for (let file of files) {
-      fs.stat(path.join(folderName, file), (err, stats) => {
+      let fullName = path.join(folderName, file);
+      fs.stat(fullName, (err, stats) => {
         if (err) throw err;
 
         if (!stats.isDirectory()) {
-          let firstCharInFilename = file[0].toUpperCase();
-          console.log(file, ' - ', firstCharInFilename);
+          copyFile(fullName);
         } else {
-          getAllFiles(folderName + '/' + file);
+          getAllFiles(fullName);
         }
       });
     }
@@ -35,15 +63,36 @@ function parseArguments () {
     .parse(process.argv);
   if (!program.inputFolder) {
     console.log('Please set input folder: \n -i folderName or \n --inputFolder=folderName');
-    process.exit(1);
+    process.exit(NO_INPUT_FOLDER_ARGUMENT);
   }
   if (!program.outputFolder) {
     console.log('Please set output folder: \n -o folderName or \n --outputFolder=folderName');
-    process.exit(1);
+    process.exit(NO_OUTPUT_FOLDER_ARGUMENT);
   }
   folders.input = program.inputFolder;
   folders.output = program.outputFolder;
 }
 
+function checkInputFolder (folder) {
+  if (!fs.existsSync(folder) || !fs.statSync(folder).isDirectory()) {
+    console.log(`There is no input folder: ${folder}`);
+    process.exit(NO_INPUT_FOLDER);
+  }
+}
+
+function makeOutputFolder (folder) {
+  if (!fs.existsSync(folder)) {
+    try {
+      fs.mkdirSync(folder);
+    } catch (err) {
+      console.log(`Output folder (${folder}) creation error.`);
+      process.exit(OUTPUT_FOLDER_CREATE_ERROR);
+    }
+  }
+}
+
 parseArguments();
+checkInputFolder(folders.input);
+makeOutputFolder(folders.output);
+console.log(`Copying files: ${folders.input} => ${folders.output}`);
 getAllFiles(folders.input);
