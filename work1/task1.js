e0d1d9f72a0256ca291e8a0eb6148a331c0bfa8e
Andrey Popov <andpop@mail.ru>
2019-02-19 00:13:13 +0300

    Work1/task1. Copy files without erasing.

--- work1/task1.js
+++ work1/task1.js
@@ -10,51 +10,6 @@ const NO_OUTPUT_FOLDER_ARGUMENT = 2;
 const NO_INPUT_FOLDER = 3;
 const OUTPUT_FOLDER_CREATE_ERROR = 4;
 
-function getOutFolderForFile (filename) {
-  let folderName = filename.charAt(0).toUpperCase();
-  return path.join(folders.output, folderName);
-}
-
-function copyFile (fullFilename) {
-  let outFolder, outputFileName;
-  let file = path.basename(fullFilename);
-  outFolder = getOutFolderForFile(file);
-  outputFileName = path.join(outFolder, file);
-  if (!fs.existsSync(outFolder)) {
-    try {
-      fs.mkdirSync(outFolder);
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
-  fs.copyFileSync(fullFilename, outputFileName);
-
-  console.log(outputFileName);
-}
-
-function getAllFiles (folderName) {
-  fs.readdir(folderName, (err, files) => {
-    if (err) {
-      console.error(`Input folder (${folders.input}) read error.`);
-      throw err;
-    }
-
-    for (let file of files) {
-      let fullName = path.join(folderName, file);
-      fs.stat(fullName, (err, stats) => {
-        if (err) throw err;
-
-        if (!stats.isDirectory()) {
-          copyFile(fullName);
-        } else {
-          getAllFiles(fullName);
-        }
-      });
-    }
-  });
-}
-
 function parseArguments () {
   const program = require('commander');
   program
@@ -89,6 +44,51 @@ function makeOutputFolder (folder) {
       process.exit(OUTPUT_FOLDER_CREATE_ERROR);
     }
   }
+}
+
+function getOutFolderForFile (filename) {
+  let folderName = filename.charAt(0).toUpperCase();
+  return path.join(folders.output, folderName);
+}
+
+function copyFile (fullFilename) {
+  let outFolder, outputFileName;
+  let file = path.basename(fullFilename);
+  outFolder = getOutFolderForFile(file);
+  outputFileName = path.join(outFolder, file);
+  if (!fs.existsSync(outFolder)) {
+    try {
+      fs.mkdirSync(outFolder);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+  fs.copyFileSync(fullFilename, outputFileName);
+
+  console.log(`${fullFilename} -> ${outputFileName}`);
+}
+
+function getAllFiles (folderName) {
+  fs.readdir(folderName, (err, files) => {
+    if (err) {
+      console.error(`Input folder (${folders.input}) read error.`);
+      throw err;
+    }
+
+    for (let file of files) {
+      let fullName = path.join(folderName, file);
+      fs.stat(fullName, (err, stats) => {
+        if (err) throw err;
+
+        if (!stats.isDirectory()) {
+          copyFile(fullName);
+        } else {
+          getAllFiles(fullName);
+        }
+      });
+    }
+  });
 }
 
 parseArguments();
