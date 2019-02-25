16e858f89f387239255da0292b8a4937e606b272
Andrey Popov <andpop@mail.ru>
2019-02-25 22:36:23 +0300

    Work3. Add index page render with skills from DB.

--- work3/controllers/index.js
+++ work3/controllers/index.js
@@ -1,9 +1,10 @@
+const skill = require('../models/skill');
+
 module.exports.get = function (req, res) {
-  res.render('pages/index');
+  res.render('pages/index', skill.getSkills());
 };
 
 module.exports.post = function (req, res) {
   console.log('POST /index');
   console.log(req.body.name, req.body.email, req.body.message);
-  // res.send(req.body.name + ' ' + req.body.email + ' ' + req.body.message);
 };
--- work3/models/skill.js
+++ work3/models/skill.js
@@ -8,3 +8,33 @@ module.exports.setSkills = function (skillsObj) {
     .set('skills.years', skillsObj.years)
     .write();
 };
+
+module.exports.getSkills = function () {
+  const defaultSkills = {
+    'age': 12,
+    'concerts': 76,
+    'cities': 30,
+    'years': 20
+  };
+  const skills = db.get('skills').value() || defaultSkills;
+  return {
+    mySkills: [
+      {
+        'number': skills.age,
+        'text': 'Возраст начала занятий на скрипке'
+      },
+      {
+        'number': skills.concerts,
+        'text': 'Концертов отыграл'
+      },
+      {
+        'number': skills.cities,
+        'text': 'Максимальное число городов в туре'
+      },
+      {
+        'number': skills.years,
+        'text': 'Лет на сцене в качестве скрипача'
+      }
+    ]
+  };
+};
