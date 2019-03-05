c86fab21e71c0f33bf8bcc7776d840f0c317ff60
Andrey Popov <andpop@mail.ru>
2019-03-05 14:49:07 +0300

    Work 4. Remove commented code from work3.

--- work4/app.js
+++ work4/app.js
@@ -16,16 +16,11 @@ const config = require('./config');
 
 app.use(koaStatic('./public'));
 
-// TODO Разобраться с обработкой ошибок
 const errorHandler = require('./libs/error');
 app.use(errorHandler);
 
 app.on('error', (err, ctx) => {
   ctx.body = `Произошла ошибка ${ctx.response.status}: ${ctx.response.message}.`;
-  // ctx.render('error', {
-  //   status: ctx.response.status,
-  //   error: ctx.response.message,
-  // });
 });
 
 const router = require('./routes');
@@ -43,32 +38,3 @@ app.listen(PORT, () => {
   }
   console.log('Server start on port: ', PORT);
 });
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(session(config.session));
-// app.use(flash());
-//
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', require('./routes/index'));
-
-/*
-// Ловим ошибку 404 и переходим в обработчик ошибок
-app.use(function (req, res, next) {
-  const err = new Error('Страница не найдена');
-  err.status = 404;
-  next(err);
-});
-*/
-
-/*
-// Обработчик ошибок
-app.use(function (err, req, res, next) {
-  // Отображаем сообщение об ошибке
-  res.status(err.status || 500);
-  res.set({ 'content-type': 'text/html; charset=utf-8' });
-  res.end('Произошла ошибка: ', { message: err.message, error: err });
-});
-*/
