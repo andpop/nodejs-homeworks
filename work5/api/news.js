1b1d839b9132ad961c4eab7676fbae515b6681aa
Andrey Popov <andpop@mail.ru>
2019-03-15 08:45:59 +0300

    Work 5. Edit user permissions.

--- work5/api/news.js
+++ work5/api/news.js
@@ -1,44 +1,5 @@
-const user = require('../models/user');
 const news = require('../models/news');
-
-module.exports.getAllUsers = function (req, res) {
-  user.getAllUsers()
-    .then(newsList => {
-      res.json(newsList);
-    })
-    .catch(err => {
-      res.status(501).json({ err: err.message });
-    });
-
-};
-
-module.exports.saveNewUser = function (req, res) {
-  // TODO Сделать проверку входных данных
-
-  const newUser = JSON.parse(req.body);
-  user.createUser(newUser)
-    .then(responseUser => {
-      res.json(responseUser);
-    })
-    .catch(err => {
-      res.status(501).json({ err: err.message });
-    });
-};
-
-module.exports.login = function (req, res) {
-  const account = JSON.parse(req.body);
-  user.getUserByUsername(account.username)
-    .then(userObj => {
-      if (userObj) {
-        res.json(userObj);
-      } else {
-        res.status(401).json({ err: 'Неправильное имя пользователя или пароль' });
-      }
-    })
-    .catch(err => {
-      res.status(501).json({ err: err.message });
-    });
-};
+const user = require('../models/user');
 
 module.exports.getAllNews = function (req, res) {
   news.getAllNews()
